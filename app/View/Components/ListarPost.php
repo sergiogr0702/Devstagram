1ea02a3daa9d0439524960c7fa908d00ca943d1f
Sergiogr0702 <sgonzalebk@alumnos.unex.es>
2023-05-28 20:26:57 +0200

    Componente creado para el listado de posts

---
+++ app/View/Components/ListarPost.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
+class ListarPost extends Component
+{
+    public $posts;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($posts)
+    {
+        $this->posts = $posts;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.listar-post');
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -74,24 +74,8 @@
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
 
-        @if ($posts->count())
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                @foreach ($posts as $post)
-                    <div>
-                        <a href="{{ route('posts.show', ['post' => $post, 'user' => $user]) }}">
-                            <img src="{{ asset('uploads') . '/' . $post->imagen }}"
-                                alt="Imagen del post {{ $post->titulo }}" />
-                        </a>
-                    </div>
-                @endforeach
-            </div>
+         <x-listar-post :posts="$posts"/>
 
-            <div class="my-10">
-                {{ $posts->links('pagination::tailwind') }}
-            </div>
-        @else
-            <p class="text-center text-gray-700 text-sm font-bold">No hay publicaciones</p>
-        @endif
     </section>
 
 @endsection
